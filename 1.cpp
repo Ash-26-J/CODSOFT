b31177bd6ef458614ad3f321aaa3647e7d002fee
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-18 00:07:19 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include<bits/stdc++.h>
+using namespace std;
 
 int main() {
     // Seed random number generator
-    std::srand(static_cast<unsigned int>(std::time(0)));
+    srand(static_cast<unsigned int>(std::time(0)));
     int randomNumber = std::rand() % 100 + 1; // Random number between 1 and 100
     int guess = 0;
 
-    std::cout << "Welcome to the Number Guessing Game!\n";
-    std::cout << "I have selected a number between 1 and 100. Try to guess it!\n";
+    cout << "Welcome to the Number Guessing Game!\n";
+    cout << "I have selected a number between 1 and 100. Try to guess it!\n";
 
     while (guess != randomNumber) {
-        std::cout << "Enter your guess: ";
-        std::cin >> guess;
+        cout << "Enter your guess: ";
+        cin >> guess;
 
         if (guess > randomNumber) {
-            std::cout << "Too high! Try again.\n";
+            out << "Too high! Try again.\n";
         } else if (guess < randomNumber) {
-            std::cout << "Too low! Try again.\n";
+            cout << "Too low! Try again.\n";
         } else {
-            std::cout << "Congratulations! You guessed the correct number!\n";
+            cout << "Congratulations! You guessed the correct number!\n";
         }
     }
 
